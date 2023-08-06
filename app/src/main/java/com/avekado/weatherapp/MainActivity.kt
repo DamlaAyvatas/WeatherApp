8f6ff51f35f963dcc447052c3e8d5891722d8464
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-08-06 22:41:09 +0300

    app compat activity imported

--- app/src/main/java/com/avekado/weatherapp/MainActivity.kt
+++ app/src/main/java/com/avekado/weatherapp/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.avekado.weatherapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.avekado.weatherapp.ui.theme.WeatherAppTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+val key = "def590d3828c48bf855185009233107"
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
