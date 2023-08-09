ec73f9502bea399a082c4d5429bcc46b2a2b1962
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-08-09 23:25:31 +0300

    location enable tooken back for another one

--- app/src/main/java/com/avekado/weatherapp/MainActivity.kt
+++ app/src/main/java/com/avekado/weatherapp/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.avekado.weatherapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 val key = "def590d3828c48bf855185009233107"
 
+private lateinit var binding: ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        requestPermission()
+    }
+    private fun requestPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(binding.root, "Permission Needed For Location!", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                    //request permission
+                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }.show()
+
+            }else{
+                //request permission
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+
+        }
 
     }
 }
--- app/src/main/java/com/avekado/weatherapp/SplashScreen.kt
+++ app/src/main/java/com/avekado/weatherapp/SplashScreen.kt
@@ -2,8 +2,10 @@ package com.avekado.weatherapp
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.avekado.weatherapp.ui.theme.WeatherAppTheme
 
@@ -24,30 +27,7 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
 
-        requestPermission()
-
     }
 
-    private fun requestPermission(){
-    val requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // Permission is granted. Continue the action or workflow in your
-                // app.
-            } else {
-                // Explain to the user that the feature is unavailable because the
-                // feature requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
-                Toast.makeText(this, "Permission needed!", Toast.LENGTH_LONG).show()
-            }
-        }
-    }
 
-    private fun locationEnable(): Boolean {
-        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-    }
 }
